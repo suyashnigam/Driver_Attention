978a2e4129d6bf0310cd6d8a1aae8ae57a5c14a0
suyash622 <suyash622@gmail.com>
2018-08-03 17:28:37 -0400

    Partial Cleanup

--- complete_pipe_line.py
+++ complete_pipe_line.py
@@ -21,8 +21,6 @@ from skimage import io
 import dlib
 
 ########################## New Dependencies ###################################
-import imutils
-from imutils import face_utils 
 import forward_ssd
 ########################## New Dependencies ###################################
 
@@ -43,16 +41,10 @@ def parse_args():
             default=0, type=int)
     parser.add_argument('--snapshot', dest='snapshot', help='Path of model snapshot.',
           default='', type=str)
-    parser.add_argument('--face_model', dest='face_model', help='Path of DLIB face detection model.',
-          default='', type=str)
     parser.add_argument('--video', dest='video_path', help='Path of video')
     parser.add_argument('--output_string', dest='output_string', help='String appended to output file')
     parser.add_argument('--n_frames', dest='n_frames', help='Number of frames', type=int)
     parser.add_argument('--fps', dest='fps', help='Frames per second of source video', type=float, default=30.)
-    parser.add_argument('--shape_model', dest='shape_model', help='Path of DLIB Face to landmark model.',
-          default='', type=str)
-    parser.add_argument('--save',dest='save',type=bool,default=True)
-    parser.add_argument('--save_freq',dest='save_freq',type=int,default=10000000000)
     parser.add_argument('--eye_mode',dest='eye_mode',type=int,default=1,help='1 for Conventional 2 for deep learning')
 
     ####################### SSD based face detection ###########################################################
@@ -76,9 +68,6 @@ if __name__ == '__main__':
     snapshot_path = args.snapshot
     out_dir = 'output/video'
     video_path = args.video_path
-    shape_path = args.shape_model
-    save = args.save
-    save_freq = args.save_freq
     eye_mode = args.eye_mode
 
     ssd_p_path = args.SSD_protoxt
@@ -94,22 +83,11 @@ if __name__ == '__main__':
     # ResNet50 structure
     model = hopenet.Hopenet(torchvision.models.resnet.Bottleneck, [3, 4, 6, 3], 66)
 
-    # Dlib CNN face detection model
-    cnn_face_detector = dlib.cnn_face_detection_model_v1(args.face_model)
-
     # HOG based face detection model
     detector = dlib.get_frontal_face_detector()
 
     #SSD based face detectiom module
     ssd_net = cv2.dnn.readNetFromCaffe(ssd_p_path,ssd_m_path)
-
-
-
-
-    ############################### Dlib face to landmark ############################
-    face_to_landmark  = dlib.shape_predictor(shape_path)
-    ############################### Dlib face to landmark ############################
-
 
     print ('Loading snapshot.')
     # Load snapshot
@@ -153,7 +131,7 @@ if __name__ == '__main__':
 
     txt_out = open('output/video/output-%s.txt' % args.output_string, 'w')
 
-    frame_num = 1
+    frame_num = 0
     frames_detected = 0
 
     print ("Recording time taken")
@@ -172,27 +150,12 @@ if __name__ == '__main__':
 
         cv2_frame = cv2.cvtColor(frame,cv2.COLOR_BGR2RGB)
         
-
-        # Dlib CNN face detect
-        # dets = cnn_face_detector(cv2_frame, 1)
-
-        # HOG based face detect
-        # dets = detector(cv2_frame,1)
-        
         print("[INFO] computing object detections...")
         ssd_net.setInput(ssd_pre_image)
         dets = ssd_net.forward()
 
-        
-        
-
         for idx, det in enumerate(dets):
-            # Get x_min, y_3min, x_max, y_max, conf
-            # x_min = det.rect.left()
-            # y_min = det.rect.top()
-            # x_max = det.rect.right()
-            # y_max = det.rect.bottom()
-            # conf = det.confidence
+        
 
             confidence = dets[0, 0, idx, 2]
             print ("Confidence ",confidence)
@@ -205,13 +168,6 @@ if __name__ == '__main__':
                 frames_detected +=1
                 bbox_width = abs(x_max - x_min)
                 bbox_height = abs(y_max - y_min)
-                # x_min -= 2 * bbox_width / 4
-                # x_max += 2 * bbox_width / 4
-                # y_min -= 3 * bbox_height / 4
-                # y_max += bbox_height / 4
-                # x_min = max(x_min, 0); y_min = max(y_min, 0)
-                # x_max = min(frame.shape[1], x_max); y_max = min(frame.shape[0], y_max)
-                # Crop image
                 img = cv2_frame[y_min:y_max,x_min:x_max]
                 width_new, height_new = img.shape[:2]
 
@@ -276,55 +232,22 @@ if __name__ == '__main__':
                         # xmin_eye = ex+x_min
                         print ('Eye detected')
                         num_eye+=1
-                    # eye = frame[ey:ey+eh,ex:ex+ew]
-                    # gaze_x,gaze_y = GazeUtil.getGaze(eye)
-                    # gaze_y+= ey
-                    # gaze_x+= ex
-                    # cv2.circle(frame, (gaze_x,gaze_y), 3, 255, -1)
-                    # eye_centre = (ex+ew/2,ey+eh/2)
-                    # gaze_centre= (gaze_x,gaze_y)
-
-                    
-                #################### Face to eyes Haar Cascade ################
-                 
-
-
-
-                # eye = np.array(eye)
-                # print (eye.size)
-                # if (eye.shape[0]>0):
-                #     left_eye_HC = eye[0] #arbitrary 
-                #     if(num_eye>0):
-                #         right_eye_HC = eye[1]
-                #     else:
-                #         right_eye_HC = np.zeros((eh,ew,3))
-                ###################### Face to eyes Haar Cascade ################
-                # if save==True:
-                #     print save
-                #     if (frame_num%save_freq==0):
-                #         cv2.imwrite(image_path + '/'+ 'Left_eye_{}.jpg'.format(frame_num), left_eye_HC) 
-                #         cv2.imwrite(image_path + '/'+'Right_eye_{}.jpg'.format(frame_num), right_eye_HC) 
-
-
-                #################### Detect Eyes  #############################
-                # right_eye = landmarks[36:42,:]
-                # left_eye = landmarks[42:48,:]
-                
-                # print np.amax(right_eye,axis=-1)
-                # print right_eye[1][0],right_eye[1][1],right_eye
-                #################### Detect Eyes ##############################
+            
 
                 else:
                 #################### Detect Eye deep learning  #################
+                    print ("Using Deep Learning for eye detection")
                     boxes,scores = forward_ssd.bbox_eye(gray)
                     num_eye = 0
-                    width_gray,height_gray = gray.shape
+                    height_gray = y_max - y_min
+                    width_gray  = x_max - x_min
                     for i in range(0,2):
-                          if scores[i]>0.5:
+                          if scores[i]>0.1:
+                              print ('Eye Detected')
                               ymin_eye.append(int(boxes[i][0]*height_gray) + y_min)
                               xmin_eye.append(int(boxes[i][1]*width_gray)  + x_min)
-                              ymax_eye = int(boxes[i][2]*height_gray) + y_min[i]
-                              xmax_eye = int(boxes[i][3]*width_gray)  + x_min[i]           
+                              ymax_eye = int(boxes[i][2]*height_gray) + y_min
+                              xmax_eye = int(boxes[i][3]*width_gray)  + x_min         
                               cv2.rectangle(frame,(xmin_eye[i],ymin_eye[i]),(xmax_eye,ymax_eye),(0,255,0),2)
                               eye.append(frame[ymin_eye[i]:ymax_eye,xmin_eye[i]:xmax_eye])
                               eye_centre.append(((xmin_eye[i]+xmax_eye)/2,(ymin_eye[i]+ymax_eye)/2))
@@ -342,7 +265,7 @@ if __name__ == '__main__':
                     gaze_centre = (gaze_x,gaze_y)
                     gaze_angle,gaze_vector = GazeUtil.GazeAngle(eye_centre[i],gaze_centre,eye_rad[i])
                     GazeUtil.draw_gaze(frame, gaze_angle[0],gaze_angle[1], tdx = gaze_centre[0], tdy= gaze_centre[1],marker=0)
-                    # GazeUtil.draw_gaze(frame, eye_angle[0],eye_angle[1], tdx = eye_centre[i][0], tdy= eye_centre[i][1],marker=1)
+                    cv2.putText(img = frame, text = "Gaze: "+ str(gaze_x)+','+str(gaze_y), org = (100,30), fontFace = cv2.FONT_HERSHEY_DUPLEX, fontScale = 1, color = (0, 0, 0))         
                     cv2.putText(img = frame, text = "Gaze Angle: "+ str(gaze_angle),org = (100,150+30*i), fontFace = cv2.FONT_HERSHEY_DUPLEX,fontScale = 1, color = (0, 0, 0)) 
                 ######################## Geometric Calculations ####################
 
@@ -357,18 +280,6 @@ if __name__ == '__main__':
 
                 # Plot expanded bounding box
                 cv2.rectangle(frame, (x_min, y_min), (x_max, y_max), (0,255,0), 1)
-                # cv2.putText(frame, text = "Confidence 0.2%f{}")
-                ################# Drawing detected eye rectangle ##########################
-                # cv2.rectangle(color,(x_min,y_min),(x_max,y_max),(0,255,0),1)
-                # cv2.rectangle(frame,(int(left[2]),int(left[3])),(int(left[0]),int(left[1])),(0,255,0),1)
-                # cv2.rectangle(frame,(int(right[2]),int(right[3])),(int(right[0]),int(right[1])),(0,255,0),1)
-                # for i in range(36,42):
-                #     cv2.circle(frame, (int(landmarks[i][0]),int(landmarks[i][1])), 1, (0, 0, 255), -1)
-                # for i in range(42,48):
-                #     cv2.circle(frame, (int(landmarks[i][0]),int(landmarks[i][1])), 1, (0, 0, 255), -1)
-
-                ################# Drawing detected eye rectangle ##########################                cv2.putText(img = frame, text = "Roll:"+ str(roll_predicted), org = (100,50), fontFace = cv2.FONT_HERSHEY_DUPLEX, fontScale = 1, color = (0, 0, 0))         
-                cv2.putText(img = frame, text = "Gaze: "+ str(gaze_x)+','+str(gaze_y), org = (100,30), fontFace = cv2.FONT_HERSHEY_DUPLEX, fontScale = 1, color = (0, 0, 0))         
                 cv2.putText(img = frame, text = "Roll:"+ str(roll_predicted), org = (100,60), fontFace = cv2.FONT_HERSHEY_DUPLEX, fontScale = 1, color = (0, 0, 0))         
                 cv2.putText(img = frame, text = "Yaw:"+ str(yaw_predicted), org = (100,90), fontFace = cv2.FONT_HERSHEY_DUPLEX, fontScale = 1, color = (0, 0, 0))         
                 cv2.putText(img = frame, text = "Pitch:"+ str(pitch_predicted), org = (100,120), fontFace = cv2.FONT_HERSHEY_DUPLEX, fontScale = 1, color = (0, 0, 0)) 
@@ -377,7 +288,6 @@ if __name__ == '__main__':
         out.write(frame)
         frame_num += 1
     txt_out.write("Meta Data: The confidence level was {} and the number of frames detected was {} out of {}, the time taked {}".format(conf,frames_detected,frame_num-1,time.time()-t))
-
-    print ('{} out of {} frames detected in time {}'.format(frames_detected,frame_num,time.time()-t))
+    print ('{} out of {} frames detected in time {}'.format(frames_detected-1,frame_num-1,time.time()-t))
     out.release()
     video.release()
